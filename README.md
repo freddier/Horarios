405963a5e85be42c2bdc45b13b3a14c5733c7e56
jorgetovar <jorgetovar621@gmail.com>
2020-11-17 20:24:14 -0500

    add CLI support
    Remove .idea files
    
    Add readme
    
    Add exception handling

--- README.md
+++ README.md
@@ -4,18 +4,22 @@ Banderas compartibles en email, redes, etc de zonas horarias
 
 ### Uso
 
-Editar variable de hora:
-
-```python
-date_to_convert = "2020-11-17 21:00:00"
-```
-
-### Ejecutar
-
 ```bash
+pip3 install click
 python3 horarios.py
 ```
 
+> Generar banderas para la fecha, ejemplo 2020-11-17 21:00:00: [date_to_convert]
+
+```bash
+python3 horarios.py --help
+```
+> 
+> Usage: horarios.py [OPTIONS]
+> Options:
+>   --date_to_convert TEXT  La fecha debe cumplir con el formato %Y-%m-%d
+                          %H:%M:%S, ejemplo: 2020-11-17 21:00:00
+  --help                  Show this message and exit.
 ### Contribuir
 
 Si alguien quiere agregar o mejorar algo, puede hacerlo directamente en este repositorio: [Horarios](https://github.com/freddier/Horarios/)
--- horarios.py
+++ horarios.py
@@ -2,70 +2,87 @@
 # 👆 Sin lo de arriba no se puede poner emojis
 import pytz
 import datetime
+import click
 
-# Mi script asume la hora de tu PC
-date_to_convert = "2020-11-17 21:00:00"
-# Recuerda: Es la hora de tu PC
 
-date_to_convert = datetime.datetime.strptime(
-    date_to_convert, "%Y-%m-%d %H:%M:%S")
+@click.command()
+@click.option('--date_to_convert', prompt='Generar banderas para la fecha, ejemplo 2020-11-17 21:00:00',
+              help='La fecha debe cumplir con el formato %Y-%m-%d %H:%M:%S, ejemplo: 2020-11-17 21:00:00')
+def print_flags(date_to_convert):
+    print(f'Hora: {date_to_convert}')
+    print("Generando bloque de banderas:\n")
 
-print(date_to_convert)
-print("Generando bloque de banderas:")
-print("")
-
-# En orden de tamaño de mercado/prioridad
-zones = [
-    ["🇲🇽", "America/Mexico_City"],
-    ["🇨🇴", "America/Bogota"],
-    ["🇵🇪", "America/Lima"],
-    ["🇨🇱", "America/Santiago"],
-    ["🇦🇷", "America/Buenos_Aires"],
-    ["🇪🇸", "Europe/Madrid"],
-    ["🇺🇾", "America/Montevideo"],
-    ["🇪🇨", "America/Guayaquil"],
-    ["🇬🇹", "America/Guatemala"],
-    ["🇸🇻", "America/El_Salvador"],
-    ["🇧🇴", "America/La_Paz"],
-    ["🇵🇾", "America/Asuncion"],
-    ["🇩🇴", "America/Santo_Domingo"],
-    ["🇵🇦", "America/Panama"],
-    ["🇨🇷", "America/Costa_Rica"],
-    ["🇭🇳", "America/Tegucigalpa"],
-    ["🇻🇪", "America/Caracas"],
-    ["🇳🇮", "America/Managua"],
-    ["🇨🇺", "Cuba"],
-    ["🇺🇸", "US/Pacific"]
-]
-
-# Inicializamos el diccionario
-times = {"00pm": "X"}
-
-# If you need Brazil:
-# ["🇧🇷","America/Sao_Paulo"]
-
-# Los timezones no están derivados de países, sino de ciudades.
-# Aunque la prioridad es por país
-
-for country in zones:
-    dtc = date_to_convert.astimezone(pytz.timezone(country[1]))
-    if country[1] == "Europe/Madrid":
-        # Imprime la hora en formato de 24 horas y una "H" al final
-        dtc = dtc.strftime("%-HH")
+    date_to_convert = parse(date_to_convert)
+    if date_to_convert is None:
+        print(
+            "La fecha no cumple con el formato requerido: intentar $> python3 horarios.py "
+            "--date_to_convert='2020-11-17 21:00:00'")
     else:
-        # Imprime la hora en formato de 12 horas PM/AM
-        dtc = dtc.strftime("%-I%p")
+        # En orden de tamaño de mercado/prioridad
+        zones = [
+            ["🇲🇽", "America/Mexico_City"],
+            ["🇨🇴", "America/Bogota"],
+            ["🇵🇪", "America/Lima"],
+            ["🇨🇱", "America/Santiago"],
+            ["🇦🇷", "America/Buenos_Aires"],
+            ["🇪🇸", "Europe/Madrid"],
+            ["🇺🇾", "America/Montevideo"],
+            ["🇪🇨", "America/Guayaquil"],
+            ["🇬🇹", "America/Guatemala"],
+            ["🇸🇻", "America/El_Salvador"],
+            ["🇧🇴", "America/La_Paz"],
+            ["🇵🇾", "America/Asuncion"],
+            ["🇩🇴", "America/Santo_Domingo"],
+            ["🇵🇦", "America/Panama"],
+            ["🇨🇷", "America/Costa_Rica"],
+            ["🇭🇳", "America/Tegucigalpa"],
+            ["🇻🇪", "America/Caracas"],
+            ["🇳🇮", "America/Managua"],
+            ["🇨🇺", "Cuba"],
+            ["🇺🇸", "US/Pacific"]
+        ]
+
+        # Inicializamos el diccionario
+        times = {}
+
+        # If you need Brazil:
+        # ["🇧🇷","America/Sao_Paulo"]
+
+        # Los timezones no están derivados de países, sino de ciudades.
+        # Aunque la prioridad es por país
+
+        for country in zones:
+            zone = country[1]
+            flag = country[0]
+            dtc = date_to_convert.astimezone(pytz.timezone(zone))
+            if zone == "Europe/Madrid":
+                # Imprime la hora en formato de 24 horas y una "H" al final
+                dtc = dtc.strftime("%-HH")
+            else:
+                # Imprime la hora en formato de 12 horas PM/AM
+                dtc = dtc.strftime("%-I%p")
+            try:
+                times[dtc] = times[dtc] + flag
+            except KeyError:
+                times[dtc] = flag
+
+            # Si el país es USA en Pacific, agregar el "PT" frente a bandera de US
+            if zone == "US/Pacific":
+                times[dtc] = times[dtc] + " PT"
+
+            times[dtc] = times[dtc] + " "
+
+        for t, c in times.items():
+            print(t.lower(), c.strip())
+
+
+def parse(date_to_convert):
     try:
-        times[dtc] = times[dtc] + country[0]
-    except KeyError:
-        times[dtc] = country[0]
+        return datetime.datetime.strptime(
+            date_to_convert, "%Y-%m-%d %H:%M:%S")
+    except ValueError:
+        return None
 
-    # Si el país es USA en Pacific, agregar el "PT" frente a bandera de US
-    if country[1] == "US/Pacific":
-        times[dtc] = times[dtc] + " PT"
 
-    times[dtc] = times[dtc] + " "
-
-for t, c in times.items():
-    if c != "X":
-        print(t.lower(), c.strip())
+if __name__ == '__main__':
+    print_flags()
